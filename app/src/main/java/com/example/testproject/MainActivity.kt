28615d375ab2c7bb49178ff5202043d02d84aec6
Ivan <ivantihu@gmail.com>
2022-04-21 22:11:03 +0300

    verification login and password

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -2,13 +2,36 @@ package com.example.testproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.testproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding: ActivityMainBinding
+    val login = "Ivan"
+    val password = "1111"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println("Test")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btIn.setOnClickListener{
+            binding.apply {
+                if (textLogin.text.isNullOrEmpty()) textLogin.error = "Input login"
+                if (textPassword.text.isNullOrEmpty()) textPassword.error = "Input password"
+                if (!textLogin.text.isNullOrEmpty()) {
+                    if (textLogin.text.toString() == login){
+                        if (!textPassword.text.isNullOrEmpty()){
+                            if (textPassword.text.toString() == password) {
+                                tvRezult.text = "Congratulation you sign in"
+                                layoutLogin.visibility = View.GONE
+                                layoutPassword.visibility = View.GONE
+                            }
+                            else tvRezult.text = "Invalid password"
+                        } else tvRezult.text = "Enter your password"
+                    } else tvRezult.text = "Invalid login"
+                } else tvRezult.text = "Enter your login and password"
+
+            }
+        }
     }
 
 }
