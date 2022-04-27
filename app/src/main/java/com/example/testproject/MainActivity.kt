b6450fcfb9a4d31ed11060abe6a023c40edf5007
Ivan <ivantihu@gmail.com>
2022-04-27 20:02:40 +0300

    make function to validate login and password

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -5,54 +5,55 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Toast
+import androidx.core.text.isDigitsOnly
+import androidx.core.widget.doAfterTextChanged
 import com.example.testproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    val login = "Ivan"
-    val password = "1111"
+    val loginIn = "Ivan"
+    val passwordIn = "111a"
+    var loginValid = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btIn.setOnClickListener{
+
+        binding.btIn.setOnClickListener {
             binding.apply {
-                if (textLogin.text.isNullOrEmpty()) textLogin.error = "Input login"
-                if (textPassword.text.isNullOrEmpty()) textPassword.error = "Input password"
-                if (!textLogin.text.isNullOrEmpty()) {
-                    if (textLogin.text.toString() == login){
-                        if (!textPassword.text.isNullOrEmpty()){
-                            if (textPassword.text.toString() == password) {
-                                tvRezult.text = "Congratulation you sign in"
-                                layoutLogin.visibility = View.GONE
-                                layoutPassword.visibility = View.GONE
-                            }
-                            else tvRezult.text = "Invalid password"
-                        } else tvRezult.text = "Enter your password"
-                    } else tvRezult.text = "Invalid login"
-                } else tvRezult.text = "Enter your login and password"
+                if (loginValid(textLogin.text.toString(), textPassword.text.toString())) {
+                    tvRezult.text = "Congratulation you sign in"
+                    layoutLogin.visibility = View.GONE
+                    layoutPassword.visibility = View.GONE
+                } else tvRezult.text = "Invalid login or password"
 
             }
         }
-        binding.textPassword.addTextChangedListener(textWatcher)
-        binding.textLogin.addTextChangedListener(textWatcher)
+
+        binding.textLogin.doAfterTextChanged {
+            loginValid = isLoginValid(it.toString())
+        }
+
+        binding.textPassword.doAfterTextChanged {
+            val rez = isPasswordValid(it.toString())
+            binding.btIn.isEnabled = rez && loginValid
+        }
     }
-    private val textWatcher = object : TextWatcher {
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+    private fun loginValid (login: String, password: String):Boolean = login == loginIn && password == passwordIn
+
+    private fun isLoginValid(text: String): Boolean = text.length > 2
+
+    private fun isPasswordValid(text: String): Boolean {
+       var hesLetter = false
+        var number: Int = 0
+
+        text.forEach {
+            if (it.isDigit()) number++
+            else if (it.isLetter()) hesLetter = true
         }
-
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-        override fun afterTextChanged(p0: Editable?) {
-            val text_login = binding.textLogin.text.toString()
-            val text_password = binding.textPassword.toString()
-            if (!text_login.isNullOrEmpty() && !text_password.isNullOrEmpty())
-                binding.btIn.isEnabled = true
-
-
-        }
+        return number > 2 && hesLetter
     }
 
 
